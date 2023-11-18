285115c700c1bd89089167b1dea0383af234475c
isLinXu <2267379130@qq.com>
2023-11-18 18:54:28 +0800

    update analyze_onnx_model.py

--- analyze_onnx_model.py
+++ analyze_onnx_model.py
@@ -1,13 +1,12 @@
+import os
 import onnx
 from collections import defaultdict
 from onnx import numpy_helper
 
-
 def get_dtype_name(tensor_dtype):
     return onnx.TensorProto.DataType.Name(tensor_dtype)
 
-
-def analyze_onnx_model(onnx_file_path):
+def analyze_onnx_model(onnx_file_path, save_to_file=False, output_file=None):
     # 加载ONNX模型
     model = onnx.load(onnx_file_path)
 
@@ -39,46 +38,69 @@ def analyze_onnx_model(onnx_file_path):
     for node in nodes:
         op_count[node.op_type] += 1
 
-    # 打印摘要信息
-    print(f"Model: {onnx_file_path}")
-    print(f"Node count: {node_count}")
-    print(f"Input count: {input_count}")
-    print(f"Output count: {output_count}")
-    print(f"Number of parameters: {num_params}")
-    print("\nOperators:")
+    # 计算算子百分比
+    op_percentage = {op_type: count / node_count * 100 for op_type, count in op_count.items()}
 
-    # 打印算子信息
+    # 计算模型大小
+    model_size = os.path.getsize(onnx_file_path)
+
+    # 准备输出信息
+    output_info = []
+
+    # 添加摘要信息
+    output_info.append(f"Model: {onnx_file_path}")
+    output_info.append(f"Node count: {node_count}")
+    output_info.append(f"Input count: {input_count}")
+    output_info.append(f"Output count: {output_count}")
+    output_info.append(f"Number of parameters: {num_params}")
+    output_info.append(f"Model size: {model_size} bytes")
+
+    # 添加算子信息
+    output_info.append("\nOperators:")
     for i, (op_type, count) in enumerate(op_count.items()):
-        print(f"{i + 1}. {op_type} (count: {count})")
+        output_info.append(f"{i + 1}. {op_type} (count: {count}, percentage: {op_percentage[op_type]:.2f}%)")
 
-    print("\nInputs:")
+    # 添加输入信息
+    output_info.append("\nInputs:")
     for i, input_tensor in enumerate(inputs):
         dtype_name = get_dtype_name(input_tensor.type.tensor_type.elem_type)
-        print(
-            f"{i + 1}. {input_tensor.name} (dtype: {dtype_name}, shape: {list(input_tensor.type.tensor_type.shape.dim)})")
+        output_info.append(f"{i + 1}. {input_tensor.name} (dtype: {dtype_name}, shape: {list(input_tensor.type.tensor_type.shape.dim)})")
 
-    print("\nOutputs:")
+    # 添加输出信息
+    output_info.append("\nOutputs:")
     for i, output_tensor in enumerate(outputs):
         dtype_name = get_dtype_name(output_tensor.type.tensor_type.elem_type)
-        print(
-            f"{i + 1}. {output_tensor.name} (dtype: {dtype_name}, shape: {list(output_tensor.type.tensor_type.shape.dim)})")
+        output_info.append(f"{i + 1}. {output_tensor.name} (dtype: {dtype_name}, shape: {list(output_tensor.type.tensor_type.shape.dim)})")
 
-    # 打印节点详细信息
-    print("\nNode Details:")
+    # 添加节点详细信息
+    output_info.append("\nNode Details:")
     for i, node in enumerate(nodes):
-        print(f"{i + 1}. {node.op_type} ({node.name})")
-        print(f"  Inputs: {len(node.input)}")
+        output_info.append(f"{i + 1}. {node.op_type} ({node.name})")
+        output_info.append(f"  Inputs: {len(node.input)}")
         for input_name in node.input:
-            print(f"    {input_name}")
-        print(f"  Outputs: {len(node.output)}")
+            output_info.append(f"    {input_name}")
+        output_info.append(f"  Outputs: {len(node.output)}")
         for output_name in node.output:
-            print(f"    {output_name}")
-        # 打印属性信息
-        print("  Attributes:")
+            output_info.append(f"    {output_name}")
+        # 添加属性信息
+        output_info.append("  Attributes:")
         for attr in node.attribute:
-            print(f"    {attr.name}: {attr}")
+            output_info.append(f"    {attr.name}: {attr}")
+
+    # 打印输出信息
+    for line in output_info:
+        print(line)
+
+    # 保存输出信息到文件
+    if save_to_file:
+        if output_file is None:
+            output_file = os.path.splitext(onnx_file_path)[0] + "_analysis.txt"
+        with open(output_file, "w") as f:
+            for line in output_info:
+                f.write(line + "\n")
+        print(f"\nModel analysis saved to {output_file}")
 
 if __name__ == '__main__':
     # 使用示例
     model_path = "/Users/gatilin/CLionProjects/opencv-inference/weights/yolov3/yolov3.onnx"
-    analyze_onnx_model(model_path)
+    analyze_onnx_model(model_path, save_to_file=True)
