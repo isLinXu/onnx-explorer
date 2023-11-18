87802ee45b1d8da68e4f003f88ce42bc8380b088
isLinXu <2267379130@qq.com>
2023-11-18 21:28:17 +0800

    update main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from onnx_explorer.OnnxAlyzer import ONNXModelAnalyzer
+
+if __name__ == '__main__':
+    print("hello world")
+    # 使用示例
+    # Usage example
+    model_path = "/Users/gatilin/CLionProjects/opencv-inference/weights/yolov3/yolov3.onnx"
+    output_file = "/Users/gatilin/PycharmProjects/onnx-easy-tools/weights/yolov3/yolov3_analysis"
+    ONNXModelAnalyzer.analyze_onnx_model(model_path, save_to_file=True, output_file=output_file, show_node_details=True)
