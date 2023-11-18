1a9d3450c338a7327ab8c684fbe2f6c8a2419a78
isLinXu <2267379130@qq.com>
2023-11-18 22:09:42 +0800

    [onnx_explorer] update __init__.py

--- onnx_explorer/utils/__init__.py
+++ onnx_explorer/utils/__init__.py
@@ -1,2 +1,16 @@
+import os
 
+def bytes_to_readable_size(size_in_bytes, unit='MB'):
+    if unit == 'MB':
+        return size_in_bytes / (1024 * 1024)
+    elif unit == 'KB':
+        return size_in_bytes / 1024
+    elif unit == 'GB':
+        return size_in_bytes / (1024 * 1024 * 1024)
+    else:
+        raise ValueError("Invalid unit. Supported units are 'MB' and 'KB'.")
 
+def get_file_size(file_path,unit='MB'):
+    file_path_size = os.path.getsize(file_path)
+    file_size_mb = bytes_to_readable_size(file_path_size, unit=unit)
+    return file_size_mb
