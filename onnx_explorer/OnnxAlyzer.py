0606f7059caa733b6abcee9c1e1c9ff7dc83df82
isLinXu <2267379130@qq.com>
2023-11-19 12:19:51 +0800

    [onnx_explorer] update OnnxAlyzer.py

--- onnx_explorer/OnnxAlyzer.py
+++ onnx_explorer/OnnxAlyzer.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 import os
 import json
@@ -125,118 +126,121 @@ class ONNXModelAnalyzer:
         :param show_node_details:
         :return:
         '''
-        # Load ONNX model
-        model = onnx.load(onnx_file_path)
+        if onnx_file_path is not None:
+            if os.path.exists(onnx_file_path):
+                # Load ONNX model
+                model = onnx.load(onnx_file_path)
 
-        # Validate model
-        onnx.checker.check_model(model)
+                # Validate model
+                onnx.checker.check_model(model)
 
-        # Get graph information
-        graph = model.graph
+                # Get graph information
+                graph = model.graph
 
-        # Get node information
-        nodes = graph.node
+                # Get node information
+                nodes = graph.node
 
-        # Get input and output tensor information
-        inputs = graph.input
-        outputs = graph.output
+                # Get input and output tensor information
+                inputs = graph.input
+                outputs = graph.output
 
-        # Get model parameters
-        initializer = graph.initializer
+                # Get model parameters
+                initializer = graph.initializer
 
-        # Count the number of nodes
-        node_count = len(nodes)
+                # Count the number of nodes
+                node_count = len(nodes)
 
-        # Count the number of input and output tensors
-        input_count = len(inputs)
-        output_count = len(outputs)
+                # Count the number of input and output tensors
+                input_count = len(inputs)
+                output_count = len(outputs)
 
-        # Calculate the number of parameters
-        num_params = sum(numpy_helper.to_array(tensor).size for tensor in initializer)
+                # Calculate the number of parameters
+                num_params = sum(numpy_helper.to_array(tensor).size for tensor in initializer)
 
-        # Count the number of operators
-        op_count = defaultdict(int)
-        for node in nodes:
-            op_count[node.op_type] += 1
+                # Count the number of operators
+                op_count = defaultdict(int)
+                for node in nodes:
+                    op_count[node.op_type] += 1
 
-        # Calculate operator percentage
-        op_percentage = {op_type: count / node_count * 100 for op_type, count in op_count.items()}
+                # Calculate operator percentage
+                op_percentage = {op_type: count / node_count * 100 for op_type, count in op_count.items()}
 
-        # Calculate model size
-        model_size = os.path.getsize(onnx_file_path)
+                # Calculate model size
+                model_size = os.path.getsize(onnx_file_path)
 
-        # Count the number of parameters for each data type
-        dtype_count = defaultdict(int)
-        for tensor in initializer:
-            dtype_name = ONNXModelAnalyzer.get_dtype_name(tensor.data_type)
-            dtype_count[dtype_name] += numpy_helper.to_array(tensor).size
+                # Count the number of parameters for each data type
+                dtype_count = defaultdict(int)
+                for tensor in initializer:
+                    dtype_name = ONNXModelAnalyzer.get_dtype_name(tensor.data_type)
+                    dtype_count[dtype_name] += numpy_helper.to_array(tensor).size
 
-        # Prepare output information
-        output_info = {
-            "summary": {
-                "model": onnx_file_path,
-                "node_count": node_count,
-                "input_count": input_count,
-                "output_count": output_count,
-                "num_params": num_params,
-                "model_size": byte_to_mb(model_size)
-            },
-            "parameter_data_types": {dtype_name: count for dtype_name, count in dtype_count.items()},
-            "operators": {op_type: {"count": count, "percentage": op_percentage[op_type]} for op_type, count in
-                          op_count.items()},
-            "operators_list": list(op_count.keys()),
-            "inputs": [{"name": input_tensor.name,
-                        "dtype": ONNXModelAnalyzer.get_dtype_name(input_tensor.type.tensor_type.elem_type),
-                        "shape": [dim.dim_value for dim in input_tensor.type.tensor_type.shape.dim]} for input_tensor in
-                       inputs],
-            "outputs": [{"name": output_tensor.name,
-                         "dtype": ONNXModelAnalyzer.get_dtype_name(output_tensor.type.tensor_type.elem_type),
-                         "shape": [dim.dim_value for dim in output_tensor.type.tensor_type.shape.dim]} for output_tensor
-                        in outputs], }
+                # Prepare output information
+                output_info = {
+                    "summary": {
+                        "model": onnx_file_path,
+                        "node_count": node_count,
+                        "input_count": input_count,
+                        "output_count": output_count,
+                        "num_params": num_params,
+                        "model_size": byte_to_mb(model_size)
+                    },
+                    "parameter_data_types": {dtype_name: count for dtype_name, count in dtype_count.items()},
+                    "operators": {op_type: {"count": count, "percentage": op_percentage[op_type]} for op_type, count in
+                                  op_count.items()},
+                    "operators_list": list(op_count.keys()),
+                    "inputs": [{"name": input_tensor.name,
+                                "dtype": ONNXModelAnalyzer.get_dtype_name(input_tensor.type.tensor_type.elem_type),
+                                "shape": [dim.dim_value for dim in input_tensor.type.tensor_type.shape.dim]} for input_tensor in
+                               inputs],
+                    "outputs": [{"name": output_tensor.name,
+                                 "dtype": ONNXModelAnalyzer.get_dtype_name(output_tensor.type.tensor_type.elem_type),
+                                 "shape": [dim.dim_value for dim in output_tensor.type.tensor_type.shape.dim]} for output_tensor
+                                in outputs], }
 
-        if show_node_details:
-            output_info["node_details"] = [
-                {
-                    "op_type": node.op_type,
-                    "name": node.name,
-                    "inputs": [input_name for input_name in node.input],
-                    "outputs": [output_name for output_name in node.output],
-                    "attributes": {attr.name: str(attr) for attr in node.attribute}
-                } for node in nodes
-            ]
+                if show_node_details:
+                    output_info["node_details"] = [
+                        {
+                            "op_type": node.op_type,
+                            "name": node.name,
+                            "inputs": [input_name for input_name in node.input],
+                            "outputs": [output_name for output_name in node.output],
+                            "attributes": {attr.name: str(attr) for attr in node.attribute}
+                        } for node in nodes
+                    ]
 
-            # Print output information
-            import pprint
-            pprint.pprint(output_info)
+                    # Print output information
+                    import pprint
+                    pprint.pprint(output_info)
 
-        # Save output information to file
-        if save_to_file:
-            if output_file is None:
-                output_file = os.path.splitext(onnx_file_path)[0] + "_analysis"
+                # Save output information to file
+                if save_to_file:
+                    if output_file is None:
+                        output_file = os.path.splitext(onnx_file_path)[0] + "_analysis"
+                    else:
+                        output_path = os.path.split(output_file)[0]
+                        print("output_path:", output_path)
+                        if os.path.exists(output_path) is False:
+                            os.makedirs(output_path)
+
+                    # Save as JSON
+                    ONNXModelAnalyzer.save_format_json(output_file, output_info)
+
+                    # Save as TXT
+                    ONNXModelAnalyzer.save_format_txt(output_info, output_file)
+
+                    # Save as CSV
+                    ONNXModelAnalyzer.save_format_csv(output_info, output_file)
             else:
-                output_path = os.path.split(output_file)[0]
-                print("output_path:", output_path)
-                if os.path.exists(output_path) is False:
-                    os.makedirs(output_path)
+                print("onnx_file_path not found")
+        else:
+            print("onnx_file_path is None")
 
-            # Save as JSON
-            ONNXModelAnalyzer.save_format_json(output_file, output_info)
-
-            # Save as TXT
-            ONNXModelAnalyzer.save_format_txt(output_info, output_file)
-
-            # Save as CSV
-            ONNXModelAnalyzer.save_format_csv(output_info, output_file)
-
-
-
+def main():
+    print(f"{logo_str}\n")
+    model_path = "../ckpts/yolov5/yolov5x6.onnx"
+    output_file = "../weights/yolov5/yolov5x6"
+    ONNXModelAnalyzer.analyze_onnx_model(model_path, save_to_file=True, output_file=output_file,
+                                         show_node_details=False)
 
 if __name__ == '__main__':
-    # Usage example
-    # model_path = "/Users/gatilin/CLionProjects/opencv-inference/weights/yolov3/yolov3-spp.onnx"
-    # output_file = "/Users/gatilin/PycharmProjects/onnx-easy-tools/weights/yolov3-spp/yolov3-spp"
-    # model_path = "/Users/gatilin/CLionProjects/opencv-inference/weights/yolov3/yolov3-tiny.onnx"
-    # output_file = "/Users/gatilin/PycharmProjects/onnx-easy-tools/weights/yolov3-tiny/yolov3"
-    model_path = "/Users/gatilin/CLionProjects/opencv-inference/weights/yolov3/yolov3.onnx"
-    output_file = "/Users/gatilin/PycharmProjects/onnx-easy-tools/weights/yolov3/yolov3"
-    ONNXModelAnalyzer.analyze_onnx_model(model_path, save_to_file=True, output_file=output_file, show_node_details=True)
+    main()
