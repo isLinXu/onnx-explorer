84b63994e547d948ac6f3d2099962e1e2bdfe93e
isLinXu <2267379130@qq.com>
2023-11-19 14:07:19 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+import argparse
+
 from onnx_explorer.OnnxAlyzer import ONNXModelAnalyzer
 from onnx_explorer import logo_str
+
 
 if __name__ == '__main__':
     print(f"{logo_str}\n")
     # 使用示例
+    '''
     # Usage example
     model_path = "/Users/gatilin/CLionProjects/opencv-inference/weights/yolov3/yolov3.onnx"
     output_file = "/Users/gatilin/PycharmProjects/onnx-easy-tools/weights/yolov3/yolov3_analysis"
     ONNXModelAnalyzer.analyze_onnx_model(model_path, save_to_file=True, output_file=output_file, show_node_details=False)
+    '''
+    # 使用 argparse 解析命令行参数
+    parser = argparse.ArgumentParser(description="Analyze ONNX model.")
+    parser.add_argument("-m","--model-path", help="Path to the ONNX model file.")
+    parser.add_argument("-s", "--save", action="store_true", help="Save analysis to a text file.")
+    parser.add_argument("-o", "--output-path", help="Path to the output text file.")
+    args = parser.parse_args()
+    model_path = args.model_path
+    save_to_file = args.save
+    output_file = args.output_path
+    ONNXModelAnalyzer.analyze_onnx_model(model_path, save_to_file=save_to_file, output_file=output_file, show_node_details=False)
