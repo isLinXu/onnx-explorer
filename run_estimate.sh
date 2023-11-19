292b467033033a31e10644b413ce0264abec9c4e
isLinXu <2267379130@qq.com>
2023-11-19 22:05:21 +0800

    update run_estimate.sh

--- run_estimate.sh
+++ run_estimate.sh
@@ -1,4 +1,8 @@
 
-
-python3 estimate.py -m ckpts/yolov5/yolov5x6.onnx -t 'onnx' -n 4 -u 'GB'
-#python3 estimate.py -p 500000000 -n 4 -u 'GB'
+MODEL_DIR=ckpts/yolov5/yolov5x6.onnx
+MDDEL_TYPE='onnx'
+NUM_MEMORY=4
+UNIT_MEMORY='GB'
+PARAMS=500000000
+python3 estimate.py -m $MODEL_DIR -t $MDDEL_TYPE -n $NUM_MEMORY -u $UNIT_MEMORY
+#python3 estimate.py -p $PARAMS -n 4 -u 'GB'
